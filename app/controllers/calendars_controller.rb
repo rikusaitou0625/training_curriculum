dd7c3b5c367e5ad2d283e4f15d6ecdfb206b23be
rikusaitou0625 <68731892+rikusaitou0625@users.noreply.github.com>
2020-08-21 18:30:40 +0900

    Revert "Merge pull request #3 from rikusaitou0625/second_training"
    This reverts commit 6cf8de722204b4021155b0743cc0bf38075b42f5, reversing
    changes made to 6713b4cf5c56bb6ead1577b931632a6f42c393ac.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -16,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -35,11 +34,7 @@ class CalendarsController < ApplicationController
       plan = @plans.map do |plan|
         plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :week => wdays[(@todays_date + x).strftime('%w').to_i], :plans => plans}
-
-     
-
+      days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans :plans}
       @week_days.push(days)
     end
 
